5b906bfea58b7b5ad1b50d3e2e6766ff1e1f5b48
Artur <arturcastropolonorte@gmail.com>
2020-08-19 09:30:01 -0300

    App updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
+
+import Header from './components/Header';
+import Gradient from './components/Gradient';
+
 function App() {
   return (
   <ThemeProvider theme={theme}>
-    <div>Hello World</div>
+    <Header />
+    <Gradient />
     <GlobalStyles />
   </ThemeProvider>
   );
